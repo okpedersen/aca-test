b29180e1b26b684209869e1c56919eb97886c537
Ole Kristian Pedersen <okpedersen@gmail.com>
2022-02-05 17:06:09 +0100

    Test autodeploy on change

--- src/web-api-test/Controllers/WeatherForecastController.cs
+++ src/web-api-test/Controllers/WeatherForecastController.cs
@@ -11,11 +11,6 @@ namespace web_api_test.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,16 +19,9 @@ namespace web_api_test.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Hello World";
         }
     }
 }
